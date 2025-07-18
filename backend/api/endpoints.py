844c71b138070ca950cc4eb61a7201a332a7f0a9
Franco Renzulli <85003859+franrenzulli@users.noreply.github.com>
2025-07-18 15:22:56 -0300

    Update endpoints.py

--- backend/api/endpoints.py
+++ backend/api/endpoints.py
@@ -1,1 +1,16 @@
 # FASTApi routes
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter()
+
+# pydantic model for user request
+class QuestionRequest(BaseModel):
+    question: str
+
+@router.post("/ask") 
+async def ask_question(request: QuestionRequest):
+
+    print(f"Pregunta recibida desde el frontend: {request.question}") 
+    return {"answer": f"Backend dice: Recibí tu pregunta '{request.question}'. ¡Todo bien por ahora!"}
